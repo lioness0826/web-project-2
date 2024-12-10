1b0240ad802dd70613b66fec993e8b340489ef64
Eric024 <horniblowe@gmail.com>
2024-12-09 23:12:56 -0500

    Careers and List Pages

---
+++ script-eric.js
@@ -0,0 +1,111 @@
+/* DOM for List Page
+Name: Eric Horniblow
+ID: N01677339
+
+Links for all the images and descriptions are below:
+
+"https://www.imdb.com/title/tt9169592/?ref_=nv_sr_srsg_7_tt_6_nm_1_in_0_q_wanda%2520sy"
+https://m.media-amazon.com/images/M/MV5BNWQ0ZTc0MmMtOTljYS00OTU1LTgxYmYtZTJjMGVmMDhiMGI3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg
+
+"https://www.imdb.com/title/tt10332256/?ref_=ls_t_9"
+https://m.media-amazon.com/images/M/MV5BMjg2Y2Q0M2MtMTBlNi00OGFlLWI5ODAtNzhjNjkwZmU3YTk3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg
+
+"https://www.imdb.com/title/tt11738792/?ref_=ls_t_4"
+https://m.media-amazon.com/images/M/MV5BYzI4Y2Y4Y2MtNThjMi00NjVhLTk5NWEtODg1YmM4OGU1ZmZkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg
+
+"https://www.imdb.com/title/tt11269704/?ref_=ls_t_30"
+https://m.media-amazon.com/images/M/MV5BMmMyNWJjMzctMzhkNS00OThjLWJkYmQtZjE2ZTEzMzcxMjFmXkEyXkFqcGc@._V1_.jpg
+
+"https://www.imdb.com/title/tt2935800/?ref_=ls_t_20"
+https://m.media-amazon.com/images/M/MV5BMjM1MzM2OTUzNF5BMl5BanBnXkFtZTgwNjc4NzI3NDE@._V1_.jpg
+
+"https://www.imdb.com/title/tt0389960/?ref_=ls_t_13"
+https://m.media-amazon.com/images/M/MV5BNjBlM2JhNWQtZTZlYi00NmRhLWI2MDUtZmViYjllYzY1ZWUyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg
+
+"https://www.imdb.com/title/tt8697266/?ref_=ls_t_11"
+https://m.media-amazon.com/images/M/MV5BMjZlMDAwM2QtODdiNS00NGIxLWEzZDgtNDA2MDI0NmRlMjQzXkEyXkFqcGc@._V1_.jpg
+
+"https://www.imdb.com/title/tt6892388/?ref_=ls_t_16"
+https://m.media-amazon.com/images/M/MV5BYTJiN2ZlZTktY2U3Ni00ODQxLWI4YzYtYTRkNzEzYmY5MmRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg
+
+"https://www.imdb.com/title/tt7057376/?ref_=ls_t_17"
+https://m.media-amazon.com/images/M/MV5BNzIyZGM2OTAtN2U0Yy00ZGUwLTlkZGQtYzBjY2RjODA3OWJhXkEyXkFqcGc@._V1_.jpg
+
+*/
+
+function setObservational() {
+
+    document.querySelector("#aOneImage").src = "/images/listshow1.jpg"
+    document.querySelector("#aTwoImage").src = "/images/listshow2.jpg"
+    document.querySelector("#aThreeImage").src = "/images/listshow3.jpg"
+
+    document.querySelector("#rating-1").textContent = "IMDB Rating: 7.3"
+    document.querySelector("#rating-2").textContent = "IMDB Rating: 7.6"
+    document.querySelector("#rating-3").textContent = "IMDB Rating: 7.4"
+
+    document.querySelector("#eric-link-1").href = "https://www.imdb.com/title/tt9169592/?ref_=nv_sr_srsg_7_tt_6_nm_1_in_0_q_wanda%2520sy"
+    document.querySelector("#eric-link-2").href = "https://www.imdb.com/title/tt10332256/?ref_=ls_t_9"
+    document.querySelector("#eric-link-3").href = "https://www.imdb.com/title/tt11738792/?ref_=ls_t_4"
+
+    document.querySelector("#eric-head-1").textContent = "Wanda Sykes: Not Normal"
+    document.querySelector("#eric-head-2").textContent = "Hannah Gadsby: Douglas"
+    document.querySelector("#eric-head-3").textContent = "Taylor Tomlinson: Quarter-Life Crisis"
+
+    document.querySelector("#eric-cap-1").textContent = "Wanda Sykes, delivers a sharp-witted and hilarious critique on the state of the world, addressing her perspective on the current political and cultural climate, which she can only describe as, well - not normal."
+    document.querySelector("#eric-cap-2").textContent = `Hannah Gadsby ("Nanette") returns for her second stand-up special and shares her latest takes on popularity, identity, language, feminism and more.`
+    document.querySelector("#eric-cap-3").textContent = `She's halfway through her 20s, and she's over it. Too old to party, too young to settle down, comedian Taylor Tomlinson takes aim at her life choices.`
+
+}
+
+function setSituational() {
+
+    document.querySelector("#aOneImage").src = "/images/listshow4.jpg"
+    document.querySelector("#aTwoImage").src = "/images/listshow5.jpg"
+    document.querySelector("#aThreeImage").src = "/images/listshow6.jpg"
+
+    document.querySelector("#rating-1").textContent = "IMDB Rating: 7.3"
+    document.querySelector("#rating-2").textContent = "IMDB Rating: 7.2"
+    document.querySelector("#rating-3").textContent = "IMDB Rating: 7.5"
+
+    document.querySelector("#eric-link-1").href = "https://www.imdb.com/title/tt11269704/?ref_=ls_t_30"
+    document.querySelector("#eric-link-2").href = "https://www.imdb.com/title/tt2935800/?ref_=ls_t_20"
+    document.querySelector("#eric-link-3").href = "https://www.imdb.com/title/tt0389960/?ref_=ls_t_13"
+
+    document.querySelector("#eric-head-1").textContent = "Michelle Wolf: Joke Show"
+    document.querySelector("#eric-head-2").textContent = "Kathleen Madigan: Madigan Again"
+    document.querySelector("#eric-head-3").textContent = "Elayne Boosler: Party of One"
+
+    document.querySelector("#eric-cap-1").textContent = "Comedian Michelle Wolf takes on outrage culture, massages, childbirth, feminism and much more (like otters) in a stand-up special from New York City."
+    document.querySelector("#eric-cap-2").textContent = "Comic Kathleen Madigan delivers new material derived from time spent with her Irish Catholic Midwest family, eating random pills out of her mother's purse, touring Afghanistan, her unparalleled love of John Denver and more."
+    document.querySelector("#eric-cap-3").textContent = "Elayne Boosler's first Showtime stand-up comedy special."
+
+}
+
+function setSelfDep() {
+
+    document.querySelector("#aOneImage").src = "/images/listshow7.jpg"
+    document.querySelector("#aTwoImage").src = "/images/listshow8.jpg"
+    document.querySelector("#aThreeImage").src = "/images/listshow9.jpg"
+
+    document.querySelector("#rating-1").textContent = "IMDB Rating: 7.2"
+    document.querySelector("#rating-2").textContent = "IMDB Rating: 6.3"
+    document.querySelector("#rating-3").textContent = "IMDB Rating: 6.3"
+
+    document.querySelector("#eric-link-1").href = "https://www.imdb.com/title/tt8697266/?ref_=ls_t_11"
+    document.querySelector("#eric-link-2").href = "https://www.imdb.com/title/tt6892388/?ref_=ls_t_16"
+    document.querySelector("#eric-link-3").href = "https://www.imdb.com/title/tt7057376/?ref_=ls_t_17"
+
+    document.querySelector("#eric-head-1").textContent = "Iliza: Elder Millennial"
+    document.querySelector("#eric-head-2").textContent = "Lynne Koplitz: Hormonal Beast"
+    document.querySelector("#eric-head-3").textContent = "Christina P: Mother Inferior"
+
+    document.querySelector("#eric-cap-1").textContent = "Newly engaged comic Iliza Shlesinger dives into undeniable truths about life at age 35, the insanity of the road traveled and what's to come."
+    document.querySelector("#eric-cap-2").textContent = "Unabashed comedian Lynne Koplitz offers a woman's take on being crazy, the benefits of childlessness and the three things all men really want."
+    document.querySelector("#eric-cap-3").textContent = "Christina Pazsitzky hits Seattle with a biting dose of reality, telling truths about her childhood, getting older and the horrors of giving birth."
+
+
+}
+
+document.querySelector("#observational").addEventListener("click", setObservational)
+document.querySelector("#situational").addEventListener("click", setSituational)
+document.querySelector("#self-deprecating").addEventListener("click", setSelfDep)
